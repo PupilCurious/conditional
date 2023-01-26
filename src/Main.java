316b4d25f267125b7cf0d18c13614b003f3466da
PupilCurious <ertugrul.bilal78@gmail.com>
2023-01-26 17:59:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 23;
+        if(number>20){
+            System.out.println("tecilli");
+        } else if (number==20) {
+            System.out.println("asker");
+        }
+        else{
+            System.out.println("yaşı tutmuyor");
+        }
+    }
+}
